3e952b99ac1e10ffcfaf6b2b2a7cc681a24d5f91
ahendroo <ahendroo1@gmail.com>
2018-10-30 05:47:55 +0700

    meteorBE

--- api/index.js
+++ api/index.js
@@ -14,27 +14,40 @@ db.connect(()=>{
 });
 
 router.get('/', function (req, res) {
-    
+
 	return res.send("<h1>Back End !</h1>");
 });
 
-// get data 
+// get data
 router.get('/data', (req, res)=>{
     var run_sql = 'SELECT * FROM tb_product INNER JOIN tb_kategori ON tb_product.kode_kategori=tb_kategori.kode_kategori';
     db.query(run_sql, (error, hasil)=>{
-        // if(error) throw error;
+
         console.log(hasil);
         res.send(hasil);
     })
 })
 
 router.get('/data/:product', (req, res)=>{
-    var perintah = 
+    var sql_run = 
     `select * from tb_product where nama_product = ?`;
-    db.query(perintah, req.params.product, (error, hasil)=>{
+    db.query(sql_run, req.params.product, (error, hasil)=>{
         if(error) throw error;
         console.log(hasil);
         res.send(hasil);
+    })
+})
+
+router.post('/data', (req, res)=>{
+    var data_product = { nama: req.body.nama_product,
+        kode_kategori: req.body.kode_product, harga: req.body.harga,
+    }
+
+    var run_sql = 'insert into tb_product set ?'
+    db.query(run_sql, data_product, (error, hasil)=>{
+        if(error) throw error;
+        console.log(hasil);
+        res.send(hasil)
     })
 })
 
